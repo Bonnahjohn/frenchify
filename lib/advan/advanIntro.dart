1005b500e7d94808eab9d12076c8ccd0748d1cdc
John Bonnah <bonnahpoku@gmail.com>
2023-04-10 06:20:20 -0700

    intermediate and advance lesson content is specify and created

--- lib/advan/advanIntro.dart
+++ lib/advan/advanIntro.dart
@@ -13,8 +13,47 @@ class AdvanIntro extends StatelessWidget {
       padding: EdgeInsets.all(16.0),
       child: SingleChildScrollView(
         child: Column(
-          children: [
-            Image.asset('assets/images/ok.png', fit: BoxFit.fitHeight)
+          children: <Widget>[
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.abc_sharp),
+                title: Text('Sentence structure'),
+                subtitle: Text(''),
+                trailing: Icon(Icons.arrow_right),
+                onTap: () {},
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.abc_sharp),
+                title: Text('Narrative Essay'),
+                subtitle: Text(''),
+                trailing: Icon(Icons.arrow_right),
+                onTap: () {},
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.abc_sharp),
+                title: Text('Descriptive Essay'),
+                subtitle: Text(''),
+                trailing: Icon(Icons.arrow_right),
+                onTap: () {},
+              ),
+            ),
+            Card(
+              elevation: 8,
+              shadowColor: Colors.blue,
+              child: ListTile(
+                leading: Icon(Icons.south_east_outlined),
+                title: Text('Letter Writing'),
+                subtitle: Text(''),
+                onTap: () {},
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            )
           ],
         ),
       ),
--- lib/inter/intermediate.dart
+++ lib/inter/intermediate.dart
@@ -13,8 +13,82 @@ class Intermediate extends StatelessWidget {
       padding: EdgeInsets.all(16.0),
       child: SingleChildScrollView(
         child: Column(
-          children: [
-            Image.asset('assets/images/ok.png', fit: BoxFit.fitHeight)
+          children: <Widget>[
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.abc_sharp),
+                title: Text('Nouns and Articles'),
+                subtitle: Text(''),
+                trailing: Icon(Icons.arrow_right),
+                onTap: () {},
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.abc_sharp),
+                title: Text('Pronouns'),
+                subtitle: Text(''),
+                trailing: Icon(Icons.arrow_right),
+                onTap: () {},
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.numbers),
+                title: Text('Verbs'),
+                subtitle: Text(''),
+                trailing: Icon(Icons.arrow_right),
+                onTap: () {},
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.abc_sharp),
+                title: Text('Tenses'),
+                subtitle: Text(''),
+                trailing: Icon(Icons.arrow_right),
+                onTap: () {},
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.timeline),
+                title: Text('conjunctions'),
+                subtitle: Text(''),
+                trailing: Icon(Icons.arrow_right),
+                onTap: () {},
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: const Icon(Icons.color_lens),
+                title: const Text('Adverbs'),
+                subtitle: const Text(""),
+                trailing: const Icon(Icons.arrow_right),
+                onTap: () {},
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.family_restroom),
+                title: Text('adjectives'),
+                subtitle: Text(''),
+                trailing: Icon(Icons.arrow_right),
+                onTap: () {},
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.abc_sharp),
+                title: Text('prepositions'),
+                subtitle: Text(''),
+                trailing: Icon(Icons.arrow_right),
+                onTap: () {},
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            )
           ],
         ),
       ),
