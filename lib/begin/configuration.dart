427e25901d3a9bbcc5afe11f6082644dc7ba3ec1
John Bonnah <bonnahpoku@gmail.com>
2023-04-13 05:47:35 -0700

    greetings section is introduction nice in the app

---
+++ lib/begin/configuration.dart
@@ -0,0 +1,10 @@
+String introText =
+    "In French culture, greetings are an important part of social interaction. When you meet someone for the first time or see someone you know, it's customary to greet them with a friendly word or phrase.";
+
+//mmmmmmmmmmmmmmmmmmmmmm othe paragraph
+String para =
+    "In French language, people like to say hello to each other when they meet. There are many different ways to say hello, and some of them are more formal or polite than others. People also like to show their friends and family that they care about them by giving them kisses on the cheek when they say hello.\n Here are some common French greetings that you might hear:";
+
+// later note mmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+String note =
+    "Remember, it's important to be polite and friendly when you say hello to someone in France!";
--- lib/begin/salutation.dart
+++ lib/begin/salutation.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: unused_import, prefer_const_constructors, file_names, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:frenchify/begin/configuration.dart';
 import 'package:frenchify/main.dart';
 
 class Salutation extends StatelessWidget {
@@ -10,7 +11,7 @@ class Salutation extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromRGBO(100, 149, 237, 1),
+        backgroundColor: Color.fromRGBO(0, 0, 128, 1),
         title: Text('Formal letter images'),
       ),
       body: SafeArea(
@@ -18,16 +19,93 @@ class Salutation extends StatelessWidget {
         padding: EdgeInsets.all(16.0),
         child: SingleChildScrollView(
           child: Column(
-            children: <Widget>[Table()],
+            children: <Widget>[
+              SelectableText(
+                introText,
+                style: TextStyle(fontSize: 16),
+              ),
+              SelectableText(
+                para,
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Tables(),
+              SizedBox(
+                height: 30,
+              ),
+              SelectableText(
+                note,
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
           ),
         ),
       )),
       floatingActionButton: FloatingActionButton(
-          backgroundColor: Color.fromRGBO(100, 149, 237, 1),
+          backgroundColor: Color.fromRGBO(0, 0, 128, 1),
           onPressed: () {
             Navigator.pop(context);
           },
           child: Icon(Icons.arrow_back)),
     );
+  }
+}
+
+//  mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm content of the table mmmmmmmmmmmmmmmmmm
+
+class Tables extends StatelessWidget {
+  const Tables({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return InteractiveViewer(
+        boundaryMargin: EdgeInsets.all(20.0),
+        child: Table(
+          border: TableBorder.all(color: Color.fromRGBO(100, 149, 237, 1)),
+          children: [
+            TableRow(children: [
+              SelectableText('Greetings'),
+              Text('Pronunciation'),
+              Text('Meaning'),
+            ]),
+            TableRow(children: [
+              Text('Bonjour'),
+              Text('bohn-joor'),
+              Text('Good day / Hello'),
+            ]),
+            TableRow(children: [
+              Text('Bonsoir'),
+              Text('bohn-swahr'),
+              Text('Good evening'),
+            ]),
+            TableRow(children: [
+              Text('Salut'),
+              Text('sah-lyoo'),
+              Text('Hi / Hey'),
+            ]),
+            TableRow(children: [
+              Text('Coucou'),
+              Text('coo-coo'),
+              Text('Hi / Hello'),
+            ]),
+            TableRow(children: [
+              Text('Comment ça va ?'),
+              Text('koh-mohn sah vah'),
+              Text('How are you?'),
+            ]),
+            TableRow(children: [
+              Text('Ça va ?'),
+              Text('sah vah'),
+              Text('Are you OK?'),
+            ]),
+            TableRow(children: [
+              Text('Bienvenue'),
+              Text('byah-ven-yoo'),
+              Text('Welcome'),
+            ]),
+          ],
+        ));
   }
 }
