d0dab36e71c3ae7980d93bef938a8a2876247ccf
John Bonnah <bonnahpoku@gmail.com>
2023-04-13 11:38:36 -0700

    beginner content image introduction initialized

--- lib/begin/begin.dart
+++ lib/begin/begin.dart
@@ -16,7 +16,8 @@ class Beginner extends StatelessWidget {
           children: <Widget>[
             Card(
               child: ListTile(
-                leading: Icon(Icons.abc_sharp),
+                leading: Image.asset('assets/images/greet.jpg',
+                    width: 60, height: 100),
                 title: Text('Greetings and Introduction'),
                 subtitle: Text('Salutation et Introduction'),
                 trailing: Icon(Icons.arrow_right),
@@ -27,7 +28,8 @@ class Beginner extends StatelessWidget {
             ),
             Card(
               child: ListTile(
-                leading: Icon(Icons.numbers),
+                leading: Image.asset('assets/images/phrase.jpg',
+                    width: 60, height: 100),
                 title: Text('Basic phrases'),
                 subtitle: Text('phrase de base'),
                 trailing: Icon(Icons.arrow_right),
@@ -38,7 +40,8 @@ class Beginner extends StatelessWidget {
             ),
             Card(
               child: ListTile(
-                leading: const Icon(Icons.color_lens),
+                leading: Image.asset('assets/images/prof.jpg',
+                    width: 60, height: 100),
                 title: const Text('Days of the Week and Month of the year'),
                 subtitle: const Text(
                     "Les jours de la semaine et Les mois de l'annee"),
@@ -51,7 +54,7 @@ class Beginner extends StatelessWidget {
             Card(
               child: ListTile(
                 leading: Image.asset('assets/images/num.jpg',
-                    width: 100, height: 500),
+                    width: 50, height: 500),
                 title: Text('Numbers and counting'),
                 subtitle: Text('Nombres et comptage'),
                 trailing: Icon(Icons.arrow_right),
@@ -62,8 +65,8 @@ class Beginner extends StatelessWidget {
             ),
             Card(
               child: ListTile(
-                leading: Image.asset('assets/images/num.jpg',
-                    width: 100, height: 500),
+                leading: Image.asset('assets/images/color.jpg',
+                    width: 60, height: 100),
                 title: Text('Colors and describing things'),
                 subtitle: Text('couleurs et description des choses'),
                 trailing: Icon(Icons.arrow_right),
@@ -74,7 +77,8 @@ class Beginner extends StatelessWidget {
             ),
             Card(
               child: ListTile(
-                leading: Icon(Icons.abc_sharp),
+                leading: Image.asset('assets/images/fam.jpg',
+                    width: 60, height: 100),
                 title: Text('Family and relationships'),
                 subtitle: Text('Famille et relations '),
                 trailing: Icon(Icons.arrow_right),
@@ -85,7 +89,8 @@ class Beginner extends StatelessWidget {
             ),
             Card(
               child: ListTile(
-                leading: Icon(Icons.abc_sharp),
+                leading: Image.asset('assets/images/time.jpg',
+                    width: 60, height: 100),
                 title: Text('Time and dates'),
                 subtitle: Text('Heure et dates'),
                 trailing: Icon(Icons.arrow_right),
@@ -96,7 +101,8 @@ class Beginner extends StatelessWidget {
             ),
             Card(
               child: ListTile(
-                leading: Icon(Icons.abc_sharp),
+                leading: Image.asset('assets/images/prof.jpg',
+                    width: 60, height: 100),
                 title: Text('Profession'),
                 subtitle: Text('Les professions'),
                 trailing: Icon(Icons.arrow_right),
@@ -107,7 +113,8 @@ class Beginner extends StatelessWidget {
             ),
             Card(
               child: ListTile(
-                leading: Icon(Icons.abc_sharp),
+                leading: Image.asset('assets/images/special.jpg',
+                    width: 60, height: 100),
                 title: Text('Public holidays and special days(Festivals)'),
                 subtitle: Text('Jours fériés et jours spéciaux Festivals'),
                 trailing: Icon(Icons.arrow_right),
@@ -118,7 +125,8 @@ class Beginner extends StatelessWidget {
             ),
             Card(
               child: ListTile(
-                leading: Icon(Icons.south_east_outlined),
+                leading: Image.asset('assets/images/nature.jpg',
+                    width: 60, height: 100),
                 title: Text('Weather and seasons'),
                 subtitle: Text('Météo et saison'),
                 onTap: () {
