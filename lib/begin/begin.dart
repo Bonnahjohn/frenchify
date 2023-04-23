edcad2f5217bdc6ba564543246201f8c96a49d5c
John Bonnah <bonnahpoku@gmail.com>
2023-04-23 12:08:39 +0800

    image is added to the content

--- lib/begin/begin.dart
+++ lib/begin/begin.dart
@@ -26,7 +26,7 @@ class Beginner extends StatelessWidget {
           children: <Widget>[
             Card(
               child: ListTile(
-                //
+                leading: Image.asset('assets/images/intro.png'),
                 title: Text('Greetings and Introduction'),
                 subtitle: Text('Salutation et Introduction'),
                 trailing: Icon(Icons.arrow_right),
@@ -36,6 +36,7 @@ class Beginner extends StatelessWidget {
             ),
             Card(
               child: ListTile(
+                leading: Image.asset('assets/images/basic.png'),
                 title: Text('Basic phrases'),
                 subtitle: Text('phrase de base'),
                 trailing: Icon(Icons.arrow_right),
@@ -47,6 +48,7 @@ class Beginner extends StatelessWidget {
             ),
             Card(
               child: ListTile(
+                leading: Image.asset('assets/images/date.png'),
                 title: const Text('Days of the Week and Month of the year'),
                 subtitle: const Text(
                     "Les jours de la semaine et Les mois de l'annee"),
@@ -59,6 +61,7 @@ class Beginner extends StatelessWidget {
             ),
             Card(
               child: ListTile(
+                leading: Image.asset('assets/images/num.png'),
                 title: Text('Numbers and counting'),
                 subtitle: Text('Nombres et comptage'),
                 trailing: Icon(Icons.arrow_right),
@@ -70,6 +73,7 @@ class Beginner extends StatelessWidget {
             ),
             Card(
               child: ListTile(
+                leading: Image.asset('assets/images/color.png'),
                 title: Text('Colors and describing things'),
                 subtitle: Text('couleurs et description des choses'),
                 trailing: Icon(Icons.arrow_right),
@@ -81,6 +85,7 @@ class Beginner extends StatelessWidget {
             ),
             Card(
               child: ListTile(
+                leading: Image.asset('assets/images/family.png'),
                 title: Text('Family and relationships'),
                 subtitle: Text('Famille et relations '),
                 trailing: Icon(Icons.arrow_right),
@@ -92,6 +97,7 @@ class Beginner extends StatelessWidget {
             ),
             Card(
               child: ListTile(
+                leading: Image.asset('assets/images/days.png'),
                 title: Text('Time and dates'),
                 subtitle: Text('Heure et dates'),
                 trailing: Icon(Icons.arrow_right),
@@ -103,6 +109,7 @@ class Beginner extends StatelessWidget {
             ),
             Card(
               child: ListTile(
+                leading: Image.asset('assets/images/job.png'),
                 title: Text('Profession'),
                 subtitle: Text('Les professions'),
                 trailing: Icon(Icons.arrow_right),
@@ -114,6 +121,7 @@ class Beginner extends StatelessWidget {
             ),
             Card(
               child: ListTile(
+                leading: Image.asset('assets/images/festival.png'),
                 title: Text('Public holidays and special days(Festivals)'),
                 subtitle: Text('Jours fériés et jours spéciaux Festivals'),
                 trailing: Icon(Icons.arrow_right),
@@ -125,6 +133,7 @@ class Beginner extends StatelessWidget {
             ),
             Card(
               child: ListTile(
+                leading: Image.asset('assets/images/days.png'),
                 title: Text('Weather and seasons'),
                 subtitle: Text('Météo et saison'),
                 trailing: Icon(Icons.arrow_right),
