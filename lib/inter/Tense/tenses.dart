5e015fd3bf6d0b991f5febaa63f640a1ad68d835
John Bonnah <bonnahpoku@gmail.com>
2023-06-22 05:13:08 -0700

    admob has been finally added to all the pages of the app

--- lib/inter/Tense/tenses.dart
+++ lib/inter/Tense/tenses.dart
@@ -89,7 +89,7 @@ class _ADDSState extends State<ADDS> {
   _initBannerAd() {
     _bannerAd = BannerAd(
         size: AdSize.banner,
-        adUnitId: 'ca-app-pub-2551767266399179/9882829802',
+        adUnitId: 'ca-app-pub-7469711478511103/3568950315',
         listener: BannerAdListener(
           onAdLoaded: (ad) {
             setState(() {
